590b23fae34b9ce0aefbcaf0d1aa2c0674baee7e
Ainur Farkh <farkh@MacBook-Air-Ainur.local>
2019-07-06 18:04:42 +0300

    Add dummy data

--- src/redux/reducers/requestReducer.js
+++ src/redux/reducers/requestReducer.js
@@ -1,7 +1,29 @@
 import { ADD_REQUEST } from '../types/requestTypes';
 
 const initialState = {
-    requests: [],
+    requests: [
+        {
+            title: 'Test request',
+            text: 'Hello world!',
+            project: 'SDT Test',
+            priority: 1,
+            date: '06.07.2019',
+        },
+        {
+            title: 'Gimme a job please :)',
+            text: 'Lorem ipsum blah-blah-blah',
+            project: 'SDT Test',
+            priority: 2,
+            date: '05.07.2019',
+        },
+        {
+            title: 'Yet another incoming request',
+            text: 'Dummy description',
+            project: 'TestProject',
+            priority: 0,
+            date: '06.07.2019',
+        },
+    ],
 };
 
 export default (state = initialState, action) => {
