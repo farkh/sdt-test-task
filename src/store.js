195a5085bd8901e063037391845a50720a651648
Ainur Farkh <farkh@MacBook-Air-Ainur.local>
2019-07-06 12:16:00 +0300

    Add & setup Redux

---
+++ src/store.js
@@ -0,0 +1,19 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import rootReducer from './redux/reducers';
+
+const persistedState = localStorage.getItem('SDTReduxState')
+	? JSON.parse(localStorage.getItem('SDTReduxState'))
+    : {};
+const middleware = [thunk];
+
+const store = createStore(
+    rootReducer,
+    persistedState,
+    compose(
+        applyMiddleware(...middleware)
+    ),
+);
+
+export default store;
